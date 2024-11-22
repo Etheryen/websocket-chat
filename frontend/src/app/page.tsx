d03ff5c1a1d988f453e714f1931066b6ff282ac4
Etheryen <lukasz.ciwoniuk@gmail.com>
2024-11-22 22:21:39 +0100

    chore(ci-cd): clean up after test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   return (
     <main className="p-8">
       <div className="flex flex-col items-center space-y-4">
-        <h1>TEST TEST TEST</h1>
         <h1 className="text-center text-4xl font-bold">
           <Link href={"/"}>Welcome to chat</Link>
         </h1>
